e6c5b1686279c69e19da9115055032f33fe0bbfd
anas <anasmpasti@hotmail.com>
2021-04-12 15:58:40 +0000

    Roolback

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -3,16 +3,11 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
-import { TranslateInterceptor } from '../assets/i18n/services/translate.interceptor';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
-  ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: TranslateInterceptor, multi: true }
   ],
   bootstrap: [AppComponent],
 })
