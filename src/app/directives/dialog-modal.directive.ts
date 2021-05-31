67b02e86c7b23f8c3c7df42043f5af6889891f22
anasmasti <anasmasti@hotmail.com>
2021-05-31 16:18:22 +0100

    Cleaning more code

--- src/app/directives/dialog-modal.directive.ts
+++
@@ -1,8 +0,0 @@
-import { Directive, ViewContainerRef } from '@angular/core';
-
-@Directive({
-  selector: '[dialogModal]',
-})
-export class DialogModalDirective {
-  constructor(public viewContainerRef: ViewContainerRef) { }
-}
