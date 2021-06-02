530f8871dc629c6b58b29d69cf11e5194bcb782a
anasmasti <anasmasti@hotmail.com>
2021-06-02 11:48:52 +0100

    Setup backend config fot Netlify deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  API_URL: 'http://localhost:8080/api/'
+  production: true,
+  API_URL: 'https://anasmasti-portfolio.herokuapp.com/api/'
 };
 
 /*
